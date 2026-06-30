e070f2cfb27e988112a9d161bbc2239bd50bdce3
innopamSolutionPlanning <innopam.solution@gmail.com>
2026-06-30 14:56:12 +0900

    auto: update Dashboard.jsx

--- app/src/pages/Dashboard.jsx
+++ app/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import * as echarts from 'echarts';
 import Icon from '../components/Icon';
+import DsSelect from '../components/DsSelect';
 import './Dashboard.css';
 
 const HOTSPOTS = [
