024040657a469cfb639658cad58b2e1d21651ce3
innopamSolutionPlanning <innopam.solution@gmail.com>
2026-07-29 13:42:17 +0900

    auto: update Sidebar.jsx

--- app/src/components/Sidebar.jsx
+++ app/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 import Icon from './Icon';
 import logo from '../assets/logo.svg';
+import { CURRENT_USER } from '../data/currentUser';
 
 const ANALYSIS_SUB = [
   { to: '/analysis/hotspot', label: '집중 구역 분석' },
