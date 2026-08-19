83c0ea42966db6fb7be37d8f772fd0fedf6c5630
innopamSolutionPlanning <innopam.solution@gmail.com>
2026-08-19 17:44:26 +0900

    auto: update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/jeju-datahub/' : '/',
   plugins: [react()],
+  server: {
+    port: Number(process.env.PORT) || 5183,
+  },
 }))
